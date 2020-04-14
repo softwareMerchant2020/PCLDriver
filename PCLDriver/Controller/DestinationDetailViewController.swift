70d5f25492a3feaca5fe5c5ae429e6974b8f40a0
Varun Nair <nairv@purdue.edu>
2020-04-14 14:36:46 -0400

    geofencing added

--- PCLDriver/Controller/DestinationDetailViewController.swift
+++ PCLDriver/Controller/DestinationDetailViewController.swift
@@ -19,6 +19,9 @@ class DestinationDetailViewController: UIViewController, MKMapViewDelegate, CLLo
     
     // the following coordinates are to be replaced with repsonses from CLgeodecoder
     
+    let GeoFencex = 40.079005462070526
+    let GeoFencey = -75.45921509767945
+    
     // SM office
     let Ax: Double = 40.04343
     let Ay: Double = -75.24482
@@ -35,7 +38,24 @@ class DestinationDetailViewController: UIViewController, MKMapViewDelegate, CLLo
     let Dx: Double = 39.9517559
     let Dy: Double = -75.192781
     
-
+    var tempVar: CLLocationCoordinate2D?
+    func getGeocodedCoordinate( addressString : String, completionHandler: @escaping(CLLocationCoordinate2D, NSError?) -> Void ) {
+        let geocoder = CLGeocoder()
+        geocoder.geocodeAddressString(addressString) { (placemarks, error) in
+            if error == nil {
+                if let placemark = placemarks?[0] {
+                    let location = placemark.location!
+                    
+                    completionHandler(location.coordinate, nil)
+                    self.tempVar = location.coordinate
+                }
+            }
+            
+            completionHandler(kCLLocationCoordinate2DInvalid, error as NSError?)
+        }
+    }
+    
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.isNavigationBarHidden = false
@@ -48,17 +68,27 @@ class DestinationDetailViewController: UIViewController, MKMapViewDelegate, CLLo
         mapViewDisplay.showsScale = true
         mapViewDisplay.showsUserLocation = true
         // Do any additional setup after loading the view.
+        
+        requestPermissionNotifications()
 
         let Acoord = CLLocationCoordinate2D(latitude: Ax, longitude: Ay)
         let Bcoord = CLLocationCoordinate2D(latitude: Bx, longitude: By)
         let Ccoord = CLLocationCoordinate2D(latitude: Cx, longitude: Cy)
         let Dcoord = CLLocationCoordinate2D(latitude: Dx, longitude: Dy)
-        let annotArr = [["title": "location 1", "latitude": Ax, "longitude": Ay],["title": "location2", "latitude": Bx, "longitude": By],["title": "location 3", "latitude": Cx, "longitude": Cy],["title": "location4", "latitude": Dx, "longitude": Dy]]
+        let annotArr = [["title":"center", "latitude":GeoFencex, "longitude": GeoFencey]] //[["title": "location 1", "latitude": Ax, "longitude": Ay],["title": "location2", "latitude": Bx, "longitude": By],["title": "location 3", "latitude": Cx, "longitude": Cy],["title": "location4", "latitude": Dx, "longitude": Dy]]
         createAnnot(locations: annotArr)
-        
         mapThis(originCoordinate: Acoord, destinationCord: Bcoord)
         mapThis(originCoordinate: Bcoord, destinationCord: Ccoord)
         mapThis(originCoordinate: Ccoord, destinationCord: Dcoord)
+        
+        let GeoFenceCenter = CLLocationCoordinate2DMake(GeoFencex, GeoFencey)
+        let geoFenceRegion = CLCircularRegion(center: GeoFenceCenter, radius: 300, identifier: "Office")
+        geoFenceRegion.notifyOnEntry = true
+        geoFenceRegion.notifyOnExit = true
+        locationManager.startMonitoring(for: geoFenceRegion)
+
+        
+        
     }
     
 
@@ -92,7 +122,7 @@ class DestinationDetailViewController: UIViewController, MKMapViewDelegate, CLLo
             //            error handling for unforseen problems such as User denying location access, or start point = end point etc.
             guard let response = response else {
                 if error != nil {
-                    print("Something is wrong :(" + error)
+                    print("Something is wrong :(")
                 }
                 return
             }
@@ -127,5 +157,98 @@ class DestinationDetailViewController: UIViewController, MKMapViewDelegate, CLLo
         self.myCurrentLoc = locVal
         
     }
+    
+    func postLocalNotifications(eventTitle:String)
+    {
+        let center = UNUserNotificationCenter.current()
+        
+        let content = UNMutableNotificationContent()
+        content.title = eventTitle
+        content.body = "You've entered a new region"
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
+        
+        let notificationRequest:UNNotificationRequest = UNNotificationRequest(identifier: "Region", content: content, trigger: trigger)
+        
+        center.add(notificationRequest, withCompletionHandler: { (error) in
+            if let error = error {
+                // Something went wrong
+                print(error)
+            }
+            else{
+                print("added")
+            }
+        })
+    }
+    
+    
+    func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion)
+    {
+        print("entered")
+        postLocalNotifications(eventTitle: "entered Office")
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didExitRegion region: CLRegion)
+    {
+        print("exited")
+        postLocalNotifications(eventTitle: "exited Office")
+    }
+    
+    func requestPermissionNotifications(){
+        let application =  UIApplication.shared
+        
+        if #available(iOS 10.0, *) {
+            // For iOS 10 display notification (sent via APNS)
+            UNUserNotificationCenter.current().delegate = self
+            
+            let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+            UNUserNotificationCenter.current().requestAuthorization(options: authOptions) { (isAuthorized, error) in
+                if( error != nil ){
+                    print(error!)
+                }
+                else{
+                    if( isAuthorized ){
+                        print("authorized")
+                        NotificationCenter.default.post(Notification(name: Notification.Name("AUTHORIZED")))
+                    }
+                    else{
+                        let pushPreference = UserDefaults.standard.bool(forKey: "PREF_PUSH_NOTIFICATIONS")
+                        if pushPreference == false {
+                            let alert = UIAlertController(title: "Turn on Notifications", message: "Push notifications are turned off.", preferredStyle: .alert)
+                            alert.addAction(UIAlertAction(title: "Turn on notifications", style: .default, handler: { (alertAction) in
+                                guard let settingsUrl = URL(string: UIApplication.openSettingsURLString) else {
+                                    return
+                                }
+                                
+                                if UIApplication.shared.canOpenURL(settingsUrl) {
+                                    UIApplication.shared.open(settingsUrl, completionHandler: { (success) in
+                                        // Checking for setting is opened or not
+                                        print("Setting is opened: \(success)")
+                                    })
+                                }
+                                UserDefaults.standard.set(true, forKey: "PREF_PUSH_NOTIFICATIONS")
+                            }))
+                            alert.addAction(UIAlertAction(title: "No thanks.", style: .default, handler: { (actionAlert) in
+                                print("user denied")
+                                UserDefaults.standard.set(true, forKey: "PREF_PUSH_NOTIFICATIONS")
+                            }))
+                            let viewController = UIApplication.shared.keyWindow!.rootViewController
+                            DispatchQueue.main.async {
+                                viewController?.present(alert, animated: true, completion: nil)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        else {
+            let settings: UIUserNotificationSettings =
+                UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+            application.registerUserNotificationSettings(settings)
+        }
+    }
+    
+    
+    
 
 }
